06acc22a72dc1e1fb306a63d0f308ec29f0ec5b0
Alexandrozavr <sanya28sem@yandex.ru>
2023-11-06 14:09:22 +0300

    optimised scaling

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -26,7 +26,6 @@ namespace WpfApp1
         }
         #region ScaleValue Depdency Property
         public static readonly DependencyProperty ScaleValueProperty = DependencyProperty.Register("ScaleValue", typeof(double), typeof(MainWindow), new UIPropertyMetadata(1.0, new PropertyChangedCallback(OnScaleValueChanged), new CoerceValueCallback(OnCoerceScaleValue)));
-
         private static object OnCoerceScaleValue(DependencyObject o, object value)
         {
             MainWindow mainWindow = o as MainWindow;
@@ -34,40 +33,32 @@ namespace WpfApp1
                 return mainWindow.OnCoerceScaleValue((double)value);
             else return value;
         }
-
         private static void OnScaleValueChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
         {
             MainWindow mainWindow = o as MainWindow;
             if (mainWindow != null)
                 mainWindow.OnScaleValueChanged((double)e.OldValue, (double)e.NewValue);
         }
-
         protected virtual double OnCoerceScaleValue(double value)
         {
             if (double.IsNaN(value))
                 return 1.0f;
-
             value = Math.Max(0.1, value);
             return value;
         }
-
         protected virtual void OnScaleValueChanged(double oldValue, double newValue) { }
-
         public double ScaleValue
         {
             get => (double)GetValue(ScaleValueProperty);
             set => SetValue(ScaleValueProperty, value);
         }
         #endregion
-
         private void MainGrid_SizeChanged(object sender, EventArgs e) => CalculateScale();
-
         private void CalculateScale()
         {
             double yScale = ActualHeight / 450f;
             double xScale = ActualWidth / 800f;
             double value = Math.Min(xScale, yScale);
-
             ScaleValue = (double)OnCoerceScaleValue(myMainWindow, value);
         }
     }
