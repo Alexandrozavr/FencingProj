a393eb6fb40fe23eaf9310dda5382fef51469ac2
Alexandrozavr <sanya28sem@yandex.ru>
2023-11-11 12:21:15 +0300

    updated

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp1
 {
@@ -20,10 +21,44 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        TimeSpan timeSpan = new TimeSpan(0, 1, 30);
+        bool timerStarted = false;
+
+        private DispatcherTimer timer;
         public MainWindow()
         {
             InitializeComponent();
+            timer = new DispatcherTimer
+            {
+                Interval = new TimeSpan(0, 0, 1)
+            };
+            timer.Tick += Timer_Tick;
         }
+
+        private void StartTimer_Click(object sender, RoutedEventArgs e)
+        {            
+            if (timerStarted) 
+            {
+                timer.Stop();
+                timerStarted = false;                
+            }
+            else
+            {
+                timerStarted = true;
+                timer.Start();
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timeSpan -= new TimeSpan(0, 0, 1);
+            if (timeSpan == new TimeSpan(0, 0, 0))
+            {
+                timer.Stop();
+            }
+            Timer.Content = timeSpan.ToString(@"mm\:ss");
+        }
+
         #region ScaleValue Depdency Property
         public static readonly DependencyProperty ScaleValueProperty = DependencyProperty.Register("ScaleValue", typeof(double), typeof(MainWindow), new UIPropertyMetadata(1.0, new PropertyChangedCallback(OnScaleValueChanged), new CoerceValueCallback(OnCoerceScaleValue)));
         private static object OnCoerceScaleValue(DependencyObject o, object value)
@@ -62,5 +97,71 @@ namespace WpfApp1
             ScaleValue = (double)OnCoerceScaleValue(myMainWindow, value);
         }
         #endregion
+
+        #region Buttons clicks
+        private void RemoveAlertBlue_Click(object sender, RoutedEventArgs e)
+        {
+            _ = int.TryParse(AlertsBlue.Text, out int alertsValue);
+            AlertsBlue.Text = (alertsValue - 1).ToString();
+        }
+
+        private void AddAlertBlue_Click(object sender, RoutedEventArgs e)
+        {
+            _ = int.TryParse(AlertsBlue.Text, out int alertsValue);
+            AlertsBlue.Text = (alertsValue + 1).ToString();
+        }
+
+        private void AddAlertRed_Click(object sender, RoutedEventArgs e)
+        {
+            _ = int.TryParse(AlertsRed.Text, out int alertsValue);
+            AlertsRed.Text = (alertsValue + 1).ToString();
+        }
+
+        private void RemoveAlertRed_Click(object sender, RoutedEventArgs e)
+        {
+            _ = int.TryParse(AlertsRed.Text, out int alertsValue);
+            AlertsRed.Text = (alertsValue - 1).ToString();
+        }
+
+        private void RemovePointFromBlue_Click(object sender, RoutedEventArgs e)
+        {
+            _ = int.TryParse(BlueFighterPointsCounter.Text, out int pointsValue);
+            BlueFighterPointsCounter.Text = (pointsValue - 1).ToString();
+        }
+
+        private void NullerBlue_Click(object sender, RoutedEventArgs e)
+        {
+
+            BlueFighterPointsCounter.Text = "0";
+        }
+
+        private void AddPointToBlue_Click(object sender, RoutedEventArgs e)
+        {
+            _ = int.TryParse(BlueFighterPointsCounter.Text, out int pointsValue);
+            BlueFighterPointsCounter.Text = (pointsValue + 1).ToString();
+        }
+
+        private void RemovePointFromRed_Click(object sender, RoutedEventArgs e)
+        {
+            _ = int.TryParse(RedFighterPointsCounter.Text, out int pointsValue);
+            RedFighterPointsCounter.Text = (pointsValue - 1).ToString();
+        }
+
+        private void NullerRed_Click(object sender, RoutedEventArgs e)
+        {
+            RedFighterPointsCounter.Text = "0";
+        }
+
+        private void AddPointToRed_Click(object sender, RoutedEventArgs e)
+        {
+            _ = int.TryParse(RedFighterPointsCounter.Text, out int pointsValue);
+            RedFighterPointsCounter.Text = (pointsValue + 1).ToString();
+        }
+        #endregion
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
